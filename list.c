e2e3dd2669b14e21a5ba6d9a7f9f53ac220dc579
Rusty Davis <zopppo@gmail.com>
2015-05-11 09:27:41 -0400

    Added linked list for chaining

--- list.c
+++ list.c
@@ -1,1 +1,148 @@
 #include "list.h"
+
+// Returns a new empty list
+list_t *newList(size_t size) {
+    // Store the list at this page address
+    list_t *newList = malloc(size);
+    newList->head = NULL;
+    newList->tail = NULL;
+    newList->size = 0;
+
+    return newList;
+}
+
+// Add a node to to the back of the list
+node_t *pushNode(list_t *list, void *data){ 
+    node_t *node;
+
+    if (list->head == NULL) {
+        // Allocate space for the node
+        node = newNode(data, NULL, NULL);
+        list->head = node;
+        // When the list has 1 element, the head and tail are equal
+        list->tail = list->head;
+        list->size++;
+    }
+    else if (list->size == 1) {
+        node = newNode(data,  NULL, list->head);	
+        list->tail = node;
+        list->head->next = list->tail;
+        list->size++;
+    }
+    else {
+        node = newNode(data, NULL, list->tail);
+        list->tail->next = node;
+        // the new node is the new tail
+        list->tail = list->tail->next;
+        list->size++;
+    }
+    return node;
+}
+
+// Adds a node to to the back of the list without creating a new node first
+void insertNode(list_t *list, node_t *newNode){ 
+    if (list->head == NULL) {
+        // Allocate space for the node
+        list->head = newNode;
+        newNode->next = NULL;
+        newNode->prev = NULL;
+        // When the list has 1 element, the head and tail are equal
+        list->tail = list->head;
+        list->size++;
+    }
+    else if (list->size == 1) {
+        list->tail = newNode;	
+        newNode->next = NULL;
+        newNode->prev = list->head;
+        list->head->next = list->tail;
+        list->size++;
+    }
+    else {
+        list->tail->next = newNode;
+        newNode->next = NULL;
+        newNode->prev = list->tail;
+        // the new node is the new tail
+        list->tail = list->tail->next;
+        list->size++;
+    }
+}
+
+// Removes a node from the back of the list
+node_t *popNode(list_t *list) {
+    node_t *doomed_node = NULL;
+    if (list->size == 0) {
+        printf("Cannot remove an item from an empty list.\n");
+        exit(1);
+    }
+
+    if (list->head == list->tail) {
+        doomed_node = list->head;
+        // Unlink the tail and head from the list
+        list->head = NULL;
+        list->tail = NULL;
+    }
+    else {
+        list->tail->prev->next = NULL;
+        doomed_node = list->tail;
+        list->tail = list->tail->prev;
+    }
+    list->size--;
+    return doomed_node;
+}
+
+// Returns a pointer to the first node whose data element matches the argument. If no element matches, return NULL
+node_t *findNode(list_t *list, void *key){
+
+    node_t *currentNode = list->head;        
+    while (currentNode != NULL && currentNode->data != key) {
+        currentNode = currentNode->next;
+    }
+    return currentNode;
+    }
+
+// Returns a pointer to a newly allocated node
+node_t *newNode(void *data, node_t *next, node_t *prev) {
+    node_t *newNode = malloc(sizeof(node_t));
+    newNode->next = next;
+    newNode->prev = prev;
+    return newNode;
+}
+
+
+// Removes the node that is at the supplied address
+void deleteNode(list_t *list, node_t *node) {
+    if (node == NULL) {
+        return;
+    }
+    if (node->prev == NULL) {
+        list->head = node->next;
+    }
+    else  {
+        if (node->prev->next != NULL) {
+            node->prev->next = node->next;
+        }
+    }
+    if (node->next == NULL) {
+        list->tail = node->prev;
+    }
+    else  {
+        if (node->next->prev != NULL)
+            node->next->prev = node->prev;
+    }
+    list->size--;
+}
+
+// Removes all elements from the list
+void clearList(list_t *list) {
+    node_t *currentNode = list->head;
+    while (currentNode != NULL) {
+        free(popNode(list));
+    }
+
+}
+
+// Deletes all of the list memebers with popNode then frees the list
+void deleteList(list_t *list) {
+    clearList(list);
+    free(list);
+}
---
+++ list.h
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifndef LIST_H
+#define LIST_H
+// A doubly linked list
+
+typedef struct node_t {
+    struct node_t *next;
+    struct node_t *prev;
+    void *data;
+} node_t;
+
+typedef struct list_t {
+    node_t *head;
+    node_t *tail;
+    size_t size;
+} list_t;
+
+
+list_t *newList(size_t size);
+// Deletes all of the list memebers with popNode then frees the list
+void deleteList(list_t *list);
+// Adds a node to the back of the list
+node_t *pushNode(list_t *list, void *data);
+// Removes node from the back of the list
+node_t *popNode(list_t *list);
+// Returns a pointer to the first node whose data element matches the argument
+// Reqruires a reference to a compare function
+node_t *findNode(list_t *list, void *address);
+// Removes the node that is at the supplied address
+void deleteNode(list_t *list, node_t *node);
+// Removes all elements from the list
+void clearList(list_t *list);
+// Returns a pointer to a node allocated when the data from the supplied node
+node_t *newNode(void *data, node_t *next, node_t *prev); 
+void printList(list_t *list);
+void insertNode(list_t *list, node_t *newNode);
+#endif
